1a8ba7762cfddf3b4298f140f75cae4250c281c0
Lalitya Dodla <lalityadodla@gmail.com>
2025-12-06 20:04:36 +0530

    Fixed package.json and resolve merge conflict

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> db20c8b62fa251dad0c1d761ff409f8a937e1e75
